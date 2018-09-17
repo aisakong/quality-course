7c24750e6f89933b7901d0bf85ce2a1f3ef8cd80
hui-ho <hui-ho@outlook.com>
2018-09-17 02:50:19 +0000

    Apply fixes from StyleCI

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * This file is part of the hui-ho/quality-course.
+ *
+ * (c) jiehui <hui-ho@outlook.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use App\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
-
     // 随机取一个月以内的时间
     $updated_at = $faker->dateTimeThisMonth();
     // 传参为生成最大时间不超过，创建时间永远比更改时间要早
