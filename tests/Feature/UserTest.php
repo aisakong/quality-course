fb74d3e3b81d8a524e96fe8ef2309bc865017e78
jiehui <hui-ho@outlook.com>
2018-09-17 10:49:48 +0800

    用户相关测试：访问登录、注册、个人页面

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Category;
+use App\Reply;
+use App\Topic;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * 用户可以查看登录页面
+     *
+     * @return void
+     */
+    public function test_a_user_can_browse_login()
+    {
+        $this->get(route('login'))->assertStatus(200);
+    }
+
+    /**
+     * 用户可以查看注册页面
+     *
+     * @return void
+     */
+    public function test_a_user_can_browse_register()
+    {
+        $this->get(route('register'))->assertStatus(200);
+    }
+
+    /**
+     * 用户可以查看其他用户的个人主页
+     *
+     * @return void
+     */
+    public function test_a_user_can_browse_other_users_homepage()
+    {
+        // 如果有一个 User
+        $user = factory(User::class)->create();
+
+        // 该 User 有 Topic
+        $category = factory(Category::class)->create();
+        $topic = factory(Topic::class)
+            ->create([
+                'user_id' => $user->id,
+                'category_id' => $category->id,
+            ]);
+
+        // 该 User 有 Reply
+        $reply = factory(Reply::class)
+            ->create([
+                'user_id' => $user->id,
+                'topic_id' => $topic->id,
+            ]);
+
+        // 页面能否正常访问
+        $response = $this->get(route('users.show', $user));
+        $response->assertStatus(200);
+
+        // 查看个人信息
+        $response->assertSee($user->name);
+
+        // 查看 ta 的话题
+        $response->assertSee($topic->title);
+
+        // 查看 ta 的回复
+        $response->assertSee($reply->content);
+    }
+}
